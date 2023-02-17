8a0a24726fc78ed64dfd6af8af6fcd8fa70c3188
richard rapando <richardrapando@gmail.com>
2023-02-17 03:52:40 -0800

    alx

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,10 +4,15 @@
  * Return:0 on success
  */
 int main(void){
-	int a=0;
-	while(a<=9){
-		printf("%d\n", a);
+	int a =  0;
+
+	while (a <=  9)
+	{
+		putchar(a);
 		a++;
 	}
+
+	putchar('\n');
+
 	return (0);
 }
