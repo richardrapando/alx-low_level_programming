c2bc3dbf1abcc2520d506ea3b9330bf18c022a8a
richard rapando <richardrapando@gmail.com>
2023-02-17 03:05:11 -0800

    alx

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,11 +7,15 @@
 int main(void)
 {
 	char letter = 'a';
+
 	while (letter <= 'z')
        	{
-		putchar(letter);
-		letter++;
+	 putchar(letter);
+
+	 letter++;
 	}
+
 	putchar('\n');
+
 	return (0);
 }
