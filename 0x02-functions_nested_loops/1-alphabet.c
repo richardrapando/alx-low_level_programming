0f0ce7ce6b8ead8e6717741d8ca4cc7f4238d56f
richardrapando <122984478+richardrapando@users.noreply.github.com>
2023-02-21 10:23:42 +0000

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include "main.h"   
+#include<main.h>  
 
 /**
  * main - Entry point
@@ -16,5 +16,5 @@ void print_alphabet(void)
 		letter++;
 	}
 	_putchar('\n');
-  return 0;
+        return 0;
 }
