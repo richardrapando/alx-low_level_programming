0f4458fffaf8e5e9f3be0976aa2a49d96b954c53
richardrapando <122984478+richardrapando@users.noreply.github.com>
2023-02-21 10:07:49 +0000

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,20 @@
-sddddjkddkdkdkd
+#include<main.h>
+
+/**
+ * main - Entry point
+ *Print alphabet in lowercase
+ * Return: 0 on success
+ */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+  return 0;
+}
