634b7e6e2dcdae46dfaf0e9f469c9ded41de3e9f
richard rapando <richardrapando@gmail.com>
2023-02-27 05:32:26 -0800

    C pointers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,3 @@
-#ifndef MAIN_H
-#define MAIN.H
-
-#include <stdio.h>
-#include <unistd.h>
-
 void reset_to_98(int *n);
 void swap_int(int *, int *b);
 int _strlen(char *s);
@@ -15,5 +9,3 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
-#endif
