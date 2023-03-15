ad98c38718ca93bd459a01ab9028ffe89bc59584
richardrapando <122984478+richardrapando@users.noreply.github.com>
2023-03-15 10:50:25 +0000

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * _isalpha - function that checks for alphabetic character
  * @c: letter
  * Return: 1 if c is a letter, lowercase or uppercase.
- *         0 if c is not a letter, lowercase or uppercase       
+ * \t\t0 if c is not a letter, lowercase or uppercase       
  */
 
 int _isalpha(int c)
