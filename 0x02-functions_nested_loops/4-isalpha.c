7380f11bda1aa82e7193088ff77fc2e3c9e92003
richardrapando <122984478+richardrapando@users.noreply.github.com>
2023-03-15 10:56:57 +0000

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _isalpha - Entry point
- * @c: the integer value it receives
- * Description: checks if alphabet
- * Return: 1 if true. 0 if false
+ * _isalpha - function that checks for alphabetic character
+ * @c: alphabetic character/number
+ * 
+ * Return: 1 if c is a letter. 0 if c is otherwise
  */
 
 int _isalpha(int c)
