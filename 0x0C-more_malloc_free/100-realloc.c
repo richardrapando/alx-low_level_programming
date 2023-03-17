9a297ec462ea233ab4593b282d28c1f931657fba
richardrapando <122984478+richardrapando@users.noreply.github.com>
2023-03-17 05:43:37 -0700

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,55 +3,53 @@
 
 /**
  * _realloc -  function that reallocates a memory block using
- malloc and free
- * @ptr:  pointer to the memory previously allocated with 
- a call to malloc: malloc(old_size)
+ * malloc and free
+ * @ptr:  pointer to the memory previously allocated
  * @old_size: size, in bytes, of the allocated space for ptr
  * @new_size: new size, in bytes of the new memory block
  *
- * Return: ptr if new_size == old_size 
- * NULL if new_size is equal to zero, and ptr is not NULL
+ * Return: If new_size == old_size - ptr.
+ *         If new_size == 0 and ptr is not NULL - NULL.
  */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *z;
-	char *same, *brim;
-	unsigned int number;
+        void *z;
+        char *same, *brim;
+        unsigned int number;
 
-	if (new_size == old_size)
-		return (ptr);
+        if (new_size == old_size)
+                return (ptr);
 
-	if (ptr == NULL)
-	{
-		z = malloc(new_size);
+        if (ptr == NULL)
+        {
+                z = malloc(new_size);
 
-		if (z == NULL)
-			return (NULL);
+                if (z == NULL)
+                        return (NULL);
 
-		return (z);
-	}
-
+                return (z);
+        }
 	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
+        {
+                free(ptr);
+                return (NULL);
+        }
 
-	same = ptr;
-	z = malloc(sizeof(*same) * new_size);
+        same = ptr;
+        z = malloc(sizeof(*same) * new_size);
 
-	if (z == NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
+        if (z == NULL)
+        {
+                free(ptr);
+                return (NULL);
+        }
 
-	brim = z;
+        brim = z;
 
-	for (number = 0; number < old_size && number < new_size; number++)
-		brim[number] = *same++;
+        for (number = 0; number < old_size && number < new_size; number++)
+                brim[number] = *same++;
 
-	free(ptr);
-	return (z);
+        free(ptr);
+        return (z);
 }
