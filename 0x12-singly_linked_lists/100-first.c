cc80e26f06378433b600f7c6bfba5632678cab98
richard rapando <richardrapando@gmail.com>
2023-03-30 00:33:38 -0700

    C - Singly linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Hare_and_Tortoise - function
+ * Return: void
+ */
+
+void Hare_and_Tortoise(void)
+{
+        printf("You're beat! and yet, you must allow,\n"
+               "I bore my house upon my back!\n");
+}
---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include <string.h>
+
+* add_node - function that adds a new node at the
+ *            beginning of a list_t list
+ * @head: pointer
+ * @str: string
+ *
+ * Return: address of the new element
+ *         Otherwise - NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+        list_t *new;
+        char *dup_str;
+        int len;
+
+        new = malloc(sizeof(list_t));
+        if (new == NULL)
+                return (NULL);
+
+        dup_str = strdup(str);
+        if (dup_str == NULL)
+        {
+                free(new);
+                return (NULL);
+        }
+
+        for (len = 0; str[len];)
+                len++;
+
+        new->str = dup_str;
+        new->len = len;
+        new->next = *head;
+
+        *head = new;
+
+        return (new);
+}
