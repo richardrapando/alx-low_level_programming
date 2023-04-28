ba83d4efd6783607d2fe6897a440afbfb1d6370c
richard rapando <richardrapando@gmail.com>
2023-04-28 08:16:57 +0300

    group project

---
+++ printf/c.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_c - function that prints characters (%c)
+ * @ar_list: list of characters
+ * Return: parameters
+ */
+
+int print_c(va_list ar_list)
+{
+        int c = va_arg(ar_list, int);
+
+        return (_putchar(c));
+}
---
+++ printf/d.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+* print_d - functions that prints decimal numbers (%d)
+* @ar_list: list of arguments
+* Return: count
+*/
+
+int print_d(va_list ar_list)
+{
+        unsigned int num_Absolute, aux_Num, count_Zero, count;
+        int numbers;
+
+        count = 0;
+
+        numbers = va_arg(ar_list, int);
+
+        if (numbers < 0)
+        {
+                num_Absolute = (numbers * -1);
+                count += _putchar(45);
+        }
+        else
+                num_Absolute = numbers;
+
+        aux_Num = num_Absolute;
+        count_Zero = 1;
+        while (aux_Num > 9)
+        {
+                aux_Num /= 10;
+                count_Zero *= 10;
+        }
+
+        while (count_Zero >= 1)
+        {
+                count += _putchar(((num_Absolute / count_Zero) % 10) + '0');
+                count_Zero /= 10;
+        }
+        return (count);
+}
