4ef56312732954c93fb453a62f3a76d33d1fa5f3
richardrapando <122984478+richardrapando@users.noreply.github.com>
2023-05-08 23:53:52 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,24 +11,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int op, wr, leng = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (leng = 0; text_content[leng];)
+			leng++;
 	}
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wr = write(op, text_content, leng);
 
-	if (o == -1 || w == -1)
+	if (op == -1 || wr == -1)
 		return (-1);
 
-	close(o);
+	close(op);
 
 	return (1);
 }
